f7273f2fef5df6395336296facca77b9ab88a109
Felicia5 <Felicia5@users.noreply.github.com>
2021-02-12 05:17:17 +0000

    First rule of aria: use native HTML: use button instead of div when your text is: apply

---
+++ src/rules/first-rule-button.js
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview Enforce heading (h1, h2, etc) elements contain accessible content.
+ * @author Ethan Cohen
+ */
+
+// ----------------------------------------------------------------------------
+// Rule Definition
+// ----------------------------------------------------------------------------
+
+import { elementType } from 'jsx-ast-utils';
+import { generateObjSchema, arraySchema } from '../util/schemas';
+import hasAccessibleChild from '../util/hasAccessibleChildApply';
+import isHiddenFromScreenReader from '../util/isHiddenFromScreenReader';
+
+const errorMessage = 'Apply text must be in button elements and not div elements.';
+
+const div = ['div'];
+
+const schema = generateObjSchema({ components: arraySchema });
+
+module.exports = {
+  meta: {
+    docs: {
+      url: 'https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules/heading-has-content.md',
+    },
+    schema: [schema],
+  },
+
+  create: (context) => ({
+    JSXOpeningElement: (node) => {
+      const options = context.options[0] || {};
+      const componentOptions = options.components || [];
+      const typeCheck = div.concat(componentOptions);
+      const nodeType = elementType(node);
+
+      // Only check 'h*' elements and custom types.
+      if (typeCheck.indexOf(nodeType) === -1) {
+        return;
+      }
+      if (hasAccessibleChild(node.parent)) {
+        return;
+      }
+      if (isHiddenFromScreenReader(nodeType, node.attributes)) {
+        return;
+      }
+
+      context.report({
+        node,
+        message: errorMessage,
+      });
+    },
+  }),
+};
