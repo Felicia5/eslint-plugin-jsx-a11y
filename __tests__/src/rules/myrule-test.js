ce30f7155e8fe7c635faf1825b1babbf5884b2a4
Felicia5 <Felicia5@users.noreply.github.com>
2021-02-17 20:28:47 +0000

    added my changes to rule, rule-test and hasAccessibleChildMyrule util file.

--- __tests__/src/rules/myrule-test.js
+++ __tests__/src/rules/myrule-test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 /**
- * @fileoverview Enforce use of button elements to display apply text
- * @author Felicia Kovacs
+ * @fileoverview  help avoid overue of div elements.
+ * @author Ethan Cohen modified heading-has-content-test.js rule by Felicia Kovacs
  */
 
 // -----------------------------------------------------------------------------
@@ -19,13 +19,57 @@ import rule from '../../../src/rules/myrule';
 const ruleTester = new RuleTester();
 
 const expectedError = {
-  message: '',
+  message: 'Headings must have content and the content must be accessible by a screen reader.',
   type: 'JSXOpeningElement',
 };
 
-ruleTester.run('myrule', rule, {
+const components = [{
+  components: ['Button'],
+}];
+
+ruleTester.run('enforce-button-on-apply-content', rule, {
   valid: [
-    { code: '<div />;' },
+    // DEFAULT ELEMENT TESTS
+    { code: '<button>apply</button>' },
+    { code: '<button>Apply</button>' },
+    { code: '<button>APPLY</button>' },
+    { code: '<button><Apply /></button>' },
+    { code: '<button>{apply}</button>' },
+    { code: '<button>{Apply}</button>' },
+    { code: '<button>{APPLY}</button>' },
+    { code: '<button>{apply.description}</button>' },
+    { code: '<button dangerouslySetInnerHTML={{ __html: "apply" }} />' },
+    { code: '<button dangerouslySetInnerHTML={{ __html: "Apply" }} />' },
+    { code: '<button dangerouslySetInnerHTML={{ __html: "APPLY" }} />' },
+    { code: '<button children={children} />' },
+
+    // CUSTOM ELEMENT TESTS FOR COMPONENTS OPTION
+    { code: '<Button>apply</Button>', options: components },
+    { code: '<Button>Apply</Button>', options: components },
+    { code: '<Button>APPLY</Button>', options: components },
+    { code: '<Button><Description /></Button>', options: components },
+    { code: '<Button>{apply}</Button>', options: components },
+    { code: '<Button>{Apply}</Button>', options: components },
+    { code: '<Button>{APPLY}</Button>', options: components },
+    { code: '<Button>{apply.description}</Button>', options: components },
+    { code: '<Button dangerouslySetInnerHTML={{ __html: "apply" }} />', options: components },
+    { code: '<Button children={children} />', options: components },
+    { code: '<button aria-hidden />' },
   ].map(parserOptionsMapper),
-  invalid: [].map(parserOptionsMapper),
+  invalid: [
+    // DEFAULT ELEMENT TESTS
+    { code: '<button />', errors: [expectedError] },
+    { code: '<button><Description aria-hidden /></button>', errors: [expectedError] },
+    { code: '<button>{undefined}</button>', errors: [expectedError] },
+    { code: '<div />', errors: [expectedError] },
+    { code: '<div>apply</div>', errors: [expectedError] },
+    { code: '<div>Apply</div>', errors: [expectedError] },
+    { code: '<div>APPLY</div>', errors: [expectedError] },
+    { code: '<div>{undefined}</div>', errors: [expectedError] },
+
+    // CUSTOM ELEMENT TESTS FOR COMPONENTS OPTION
+    { code: '<Button />', errors: [expectedError], options: components },
+    { code: '<Button><Bar aria-hidden /></Button>', errors: [expectedError], options: components },
+    { code: '<Button>{undefined}</Button>', errors: [expectedError], options: components },
+  ].map(parserOptionsMapper),
 });
--- src/rules/myrule.js
+++ src/rules/myrule.js
@@ -1,28 +1,49 @@
 /**
- * @fileoverview Enforce use of button elements to display apply text
- * @author Felicia Kovacs
- * @flow
+ * @fileoverview help avoid overue of div elements.
+ * @author Ethan Cohen modified heading-has-content.js rule by Felicia Kovacs
  */
 
 // ----------------------------------------------------------------------------
 // Rule Definition
 // ----------------------------------------------------------------------------
 
-import type { JSXOpeningElement } from 'ast-types-flow';
-import { generateObjSchema } from '../util/schemas';
+import { elementType } from 'jsx-ast-utils';
+import { generateObjSchema, arraySchema } from '../util/schemas';
+import hasAccessibleChild from '../util/hasAccessibleChildMyrule';
+import isHiddenFromScreenReader from '../util/isHiddenFromScreenReader';
 
-const errorMessage = '';
+const errorMessage = 'Apply text must be in button elements and not div elements.';
 
-const schema = generateObjSchema();
+const div = ['div'];
+
+const schema = generateObjSchema({ components: arraySchema });
 
 module.exports = {
   meta: {
-    docs: {},
+    docs: {
+      url: 'https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules/heading-has-content.md',
+    },
     schema: [schema],
   },
 
-  create: (context: ESLintContext) => ({
-    JSXOpeningElement: (node: JSXOpeningElement) => {
+  create: (context) => ({
+    JSXOpeningElement: (node) => {
+      const options = context.options[0] || {};
+      const componentOptions = options.components || [];
+      const typeCheck = div.concat(componentOptions);
+      const nodeType = elementType(node);
+
+      // Only check 'h*' elements and custom types.
+      if (typeCheck.indexOf(nodeType) === -1) {
+        return;
+      }
+      if (hasAccessibleChild(node.parent)) {
+        return;
+      }
+      if (isHiddenFromScreenReader(nodeType, node.attributes)) {
+        return;
+      }
+
       context.report({
         node,
         message: errorMessage,
